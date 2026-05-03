b664ecb136642c12a1e80c57c9a567ab07a5b9ac
RandienX <delz6969@gmail.com>
2026-05-03 11:57:45 +0200

    Delete scenes directory

--- scenes/ui/game_menu/menu.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-class_name Menu
-
-var layer_down = 0
-@onready var categories = $MarginContainer/HSplitContainer/categories
-
-func _ready() -> void:
-	pass # Replace with function body.
